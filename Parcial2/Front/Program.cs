a7baabc3c221972be3d8c874c3106da083288407
jU4Nii <juanibonora2004@gmail.com>
2025-08-25 18:47:07 -0300

    PreparaciónEsqueleto

---
+++ Parcial2/Front/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Front
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a ");
+            string eleccion = IngresarString("Elija una opción:\n1.\n2.\n3.\n4.\n5.\n6.\n7.\n8.\n9.");
+
+
+            Console.Clear();
+
+            switch (eleccion)
+            {
+                case "1":
+                    
+                    Console.WriteLine("\nPresione una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+
+                case "2":
+
+                    Console.WriteLine("\nPresione una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+
+                case "3":
+
+                    Console.WriteLine("\nPresione una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+
+                case "4":
+
+                    Console.WriteLine("\nPresione una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+
+                case "5":
+
+                    Console.WriteLine("\nPresione una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+
+                case "6":
+
+                    Console.WriteLine("\nPresione una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+               
+            }
+
+            Console.Clear();
+
+        }
+
+        public static int PedirEntero(string mensaje)
+        {
+
+            int num;
+            Console.WriteLine(mensaje);
+            while (int.TryParse(Console.ReadLine(), out num) == false)
+            {
+
+                Console.WriteLine("Valor incorrecto, ingreselo nuevamente:");
+
+            }
+
+            return num;
+
+        }
+
+        public static DateTime PedirFecha(string mensaje)
+        {
+            DateTime fecha;
+            Console.WriteLine(mensaje);
+            string input = Console.ReadLine();
+
+            while (DateTime.TryParseExact(input, "dd/MM/yyyy", null,
+                    System.Globalization.DateTimeStyles.None, out fecha) == false)
+            {
+                Console.WriteLine("Fecha inválida, ingrese nuevamente (dd/MM/yyyy):");
+                input = Console.ReadLine();
+            }
+
+            return fecha;
+        }
+
+        //Se usa: DateTime fechaViaje = PedirFecha("Ingrese la fecha del viaje (dd/MM/yyyy):");
+        //Console.WriteLine("Fecha ingresada: " + fechaViaje.ToShortDateString());
+
+        
+
+        public static string IngresarString(string mensaje)
+        {
+
+            Console.WriteLine(mensaje);
+            string palabra = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(palabra) == true)
+            {
+
+                Console.WriteLine("Valor incorrecto, ingreselo nuevamente:");
+                palabra = Console.ReadLine();
+
+            }
+
+            return palabra;
+
+        }
+
+
+    }
+}
